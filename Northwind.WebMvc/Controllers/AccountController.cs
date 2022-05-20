09558ca69dfc4af0d66eba051527e577f95d5bb4
iskyldrm <isakyildirim@outlook.com>
2022-05-20 19:31:13 +0300

    Register ekranı geliştirildi

--- Northwind.WebMvc/Controllers/AccountController.cs
+++ Northwind.WebMvc/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Northwind.WebMvc.Identity;
 using Northwind.WebMvc.Models;
+using System.Text;
 
 namespace Northwind.WebMvc.Controllers
 {
@@ -60,6 +61,62 @@ namespace Northwind.WebMvc.Controllers
         {
             RegisterModel registerModel = new RegisterModel();
             return View(registerModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel register)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(register);
+            }
+            else
+            {
+                MyUser user = new MyUser();
+                user.Email = register.Email;
+                user.TCKimlik = register.TcKimlik;
+
+                var result = await userManager.CreateAsync(user, register.Password);
+                if (result.Succeeded)
+                {
+                    var code = await userManager.GenerateEmailConfirmationTokenAsync(user);
+                    StringBuilder mailbuilder = new StringBuilder();
+                    mailbuilder.Append("<html>");
+                    mailbuilder.Append("<head>");
+                    mailbuilder.Append("<meta charset='" + "utf-8'" + " />");
+                    mailbuilder.Append("<title> Email Onaylama</title>");
+                    mailbuilder.Append("</head>");
+                    mailbuilder.Append("<body>");
+
+                    mailbuilder.Append($"<p> Merhaba {user.UserName} </p><br/>");
+                    mailbuilder.Append($"Uyelik işlemleri icin aşagidaki linki tiklayin  <br/>");
+
+                    mailbuilder.Append($"<a href='http://localhost:11184/ConfirmEmail/?uid={user.Id}&code={code}'> Onaylayin</a>");
+
+                    mailbuilder.Append("</body>");
+
+                    mailbuilder.Append("</html>");
+
+                    EmailHelper helper = new EmailHelper();
+
+                    bool isSend = helper.SendEmail(user.Email, mailbuilder.ToString());
+
+                    if (isSend)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Mail gonderilemedi");
+                        return View(register);
+                    }
+                }
+                else
+                {
+                    var error = result.Errors.FirstOrDefault();
+                    ModelState.AddModelError("", error.Description);
+                    return View(register);
+                }
+            }
         }
     }
 }
